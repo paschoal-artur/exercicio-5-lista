650769e3c47a6665976aee4d978d1238ab017f7a
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-03-27 22:46:13 -0300

    Código Adicionado

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+#Input para usuário digitar o valor que deseja converter
+valor = float(input("Digite o valor em metros que deseja converter: "))
 
+#Conversão de metros para centímetros = multiplicar o valor por 100
+operação = float(valor * 100)
+
+#Printando o valor de volta para o usuário
+print("Em centímetros esse valor é " + str(operação) + ".")
